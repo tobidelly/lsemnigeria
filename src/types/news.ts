b164818837cee8cdbfe8439a69bf9e554ffe1e53
TD_SoftwareEngr <tobidelly@gmail.com>
2025-01-15 03:42:12 +0100

    News

---
+++ src/types/news.ts
@@ -0,0 +1,10 @@
+export interface NewsArticle {
+    id: string;
+    title: string;
+    date: string;
+    excerpt: string;
+    content: string;
+    image: string;
+    category: string;
+    url: string;
+  }
